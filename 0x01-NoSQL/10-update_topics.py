2a9459a4a4034125cae013700583b24fe93a2fe5
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-01-24 00:40:43 +0000

    10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,8 +5,7 @@ topics of a school document based on the name"""
 
 def update_topics(mongo_collection, name, topics):
     """update topic method"""
-    mongo_collection.update(
+    mongo_collection.update_many(
             {"name": name},
             {$set: {"topics": topics}},
-            {multi: true}
     )
